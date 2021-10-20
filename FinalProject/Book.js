87446e7cb6367c09a57721b0064756d8e8b4fd60
Abhishek <abhishekdhingra1592@gmail.com>
2021-10-19 23:33:45 -0400

    Add project files.

---
+++ FinalProject/Book.js
@@ -0,0 +1,18 @@
+﻿function ClearAll() {
+    document.getElementById("name").value = "";
+    document.getElementById("number").value = "";
+    document.getElementById("event").value = "";
+}
+
+function SetCookies() {
+    document.cookie = "name=" + '' + document.getElementById("name").value + "|number=" + document.getElementById("number").value + "|event=" + document.getElementById("event").value;
+}
+
+function GetCookies() {
+    var array = document.cookie.split('|');
+    for (var I = 0; I < array.length; I++) {
+        var det = array[I].split('=');
+        document.getElementById(det[0]).innerHTML = det[1];
+        console.log(det);
+    }
+}
